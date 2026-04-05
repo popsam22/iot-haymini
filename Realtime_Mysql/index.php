e0cec641e6cf5996dbacff77fa7406bd0539a003
popsam22 <sobiechie7@gmail.com>
2026-04-05 12:19:17 +0100

    fix: add senstive files to gitignore

--- Realtime_Mysql/index.php
+++ Realtime_Mysql/index.php
@@ -1,17 +1,6 @@
+<?php echo '<p>Hello World</p>'; ?>
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
-echo json_encode([
-    'service' => 'Haymini IoT API',
-    'status' => 'running',
-    'version' => '1.0.0'
-]);
-exit;
+ob_start();
+phpinfo();
+$info = ob_get_clean();
+echo $info;
