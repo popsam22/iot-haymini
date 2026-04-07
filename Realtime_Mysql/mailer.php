cf38252c05dbcb76135cdb8bc88c2aef69b31e5e
popsam22 <sobiechie7@gmail.com>
2026-04-07 09:04:30 +0100

    fix: smtp debug

--- Realtime_Mysql/mailer.php
+++ Realtime_Mysql/mailer.php
@@ -14,7 +14,10 @@
 
     $mail = new PHPMailer(true);
     try {
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;     
+    $mail->Debugoutput = function($str, $level) {
+    error_log("PHPMailer: " . $str);
+    };                 
     $mail->isSMTP();                                                 
     $mail->Host       = $_ENV['MAILER_HOST'];             
     $mail->SMTPAuth   = true;                                                   
